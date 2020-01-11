883f9190170ca67cc2900dc6a0fad08e0b674bbf
CharlesOnesti <35116629+CharlesOnesti@users.noreply.github.com>
2020-01-11 15:46:54 -0500

    Charles Onesti

--- personal-assistant/src/App.js
+++ personal-assistant/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Input from './form'
+import Nav from './nav-bar'
+import Greet from './greeting'
+import {ThemeProvider} from 'styled-components'
+import theme from './theme'
+import {BrowserRouter as Router, Switch, Route, Link, useParams} from "react-router-dom";
 
 const App = () => (
-  <div>
-    Beep boop hi there, I&apos;m your personal assistant hehe
-  </div>
+<ThemeProvider theme={theme}>
+  <Router>
+    <Nav/>
+    <Switch>
+      <Route exact path="/home">
+        <Greet />
+      </Route>
+      <Route path="/pa">
+            <Input/>
+      </Route>
+    </Switch>
+  </Router>
+</ThemeProvider>
+
 )
 
 export default App
